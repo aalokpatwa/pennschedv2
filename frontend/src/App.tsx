a3572beaad263f8ecf2fa99aca7c2f40283f05b8
BEW111 <bewilliams111@gmail.com>
2022-03-20 21:55:48 -0400

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <h1>hi</h1>
+  )
+}
