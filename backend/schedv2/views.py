79907e0b205fa71d4323497a7b097346cb5b587d
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2022-03-28 09:09:52 -0400

    Serializer / viewsets framework

--- backend/schedv2/views.py
+++ backend/schedv2/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from backend.schedv2.models import Course, Section, Meeting
+from backend.schedv2.serializers import CourseSerializer, SectionSerializer, MeetingSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for courses
+    """
+    queryset = Course.objects.all().order_by('full_code')
+    serializer_class = CourseSerializer
