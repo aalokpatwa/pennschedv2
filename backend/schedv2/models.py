19357ca92d6e239087ff8c8d0c3696b04886624d
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2022-03-27 22:25:01 -0400

    Added unique constraints + on_delete(s)

--- backend/schedv2/models.py
+++ backend/schedv2/models.py
@@ -3,34 +3,73 @@ from django.db import models
 class Course(models.Model):
     dept = models.fields.CharField(
         max_length=4,
+        null=False,
+        blank=False
     )
+
     number = models.fields.IntegerField(
-        max_length=4,
-    )
-    full_code = models.fields.CharField(
-        unique=True,
-    )
-
-
-class Section(models.Model):
-    # foreign key to course
-    section = models.ForeignKey("Course")
-    code = models.fields.IntegerField(
         max_length=4,
         null=False,
         blank=False
     )
 
-    # many-to-many to co-requirement sections
-    instructor = models.fields.CharField()
+    full_code = models.fields.CharField(
+        max_length=10,
+        unique=True,
+        null=False,
+        blank=False
+    )
+
+
+class Section(models.Model):
+    class Meta:
+        models.constraints.UniqueConstraint(
+            'code',
+            'course',
+            name="code_course_unique_constraint"
+        )
+
+    section = models.ForeignKey(
+        "Course",
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+
+    code = models.fields.IntegerField(
+        max_length=4,
+        null=False,
+        blank=False,
+    )
+
+    instructor = models.fields.CharField(
+        null=False
+    )
+
     recitations = models.ManyToManyField("self")
     labs = models.ManyToManyField("self")
 
 
 class Meeting(models.Model):
-    section = models.ForeignKey("Section")
-    days = models.fields.CharField(
-        max_length=7
+    section = models.ForeignKey(
+        "Section",
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
     )
-    start = models.fields.TimeField()
-    end = models.fields.TimeField()
+
+    days = models.fields.CharField(
+        max_length=7,
+        help_text="String of days MTWRF" # TODO: determine if need sat/sun
+    )
+
+    start = models.fields.TimeField(
+        null=False,
+        blank=False
+    )
+
+    end = models.fields.TimeField(
+        null=False,
+        blank=False
+    )
+
