6363f5e691c77d2117811cf764449ee4cb798913
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2022-04-11 00:22:09 -0400

    register models w admin site

--- backend/schedv2/admin.py
+++ backend/schedv2/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from schedv2.models import Course, Section, Meeting
 
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(Meeting)
