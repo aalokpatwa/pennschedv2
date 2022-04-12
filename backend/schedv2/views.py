d350ba0e118af20932f77bb3ab6ce255b980dc65
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2022-04-11 22:50:40 -0400

    Viewsets outputs schedules from main.py

--- backend/schedv2/views.py
+++ backend/schedv2/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
 from rest_framework import permissions, mixins, viewsets, generics
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from schedv2.models import Course, Section, Meeting
 from schedv2.serializers import CourseSerializer, SectionSerializer, MeetingSerializer
 from algo.main import main, rank_morningtime
-from rest_framework.parsers import JSONParser
 
 class ScheduleCreatorViewSet(
     mixins.ListModelMixin,
@@ -15,7 +12,7 @@ class ScheduleCreatorViewSet(
     serializer_class = SectionSerializer
 
     def create_schedule(self, request):
-        Response(main(self.get_queryset(), rank_morningtime, None), content_type="application/json")
+        return Response(data=main(self.get_queryset(), rank_morningtime, None), content_type="application/json")
 
     def get_queryset(self):
         """
